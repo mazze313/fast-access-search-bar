15e9b09b8edf6b5aee3f014bb3494a7da2d33447
Matthias Braun <matthiasbraun@mail.de>
2024-10-16 12:34:37 +0200

    fixed input focus and removed unused code for external file loading

--- fast-access-search-bar/popup/fast-access-search-bar.js
+++ fast-access-search-bar/popup/fast-access-search-bar.js
@@ -6,9 +6,6 @@ const html_overlay_text    = document.querySelector(".outer-wrapper .overlay-tex
 const html_search_string   = document.querySelector(".outer-wrapper .search-string"); 
 const html_search_select   = document.querySelector(".outer-wrapper .search-select");
 const html_delete_search   = document.querySelector(".outer-wrapper .delete-icon");
-const html_file_dialog     = document.querySelector(".outer-wrapper .file-wrapper .file-dialog");
-const html_file_button     = document.querySelector(".outer-wrapper .file-wrapper .file-button"); 
-const html_file_preview     = document.querySelector(".outer-wrapper .file-wrapper .file-preview"); 
 
 /* ************************************************************************* */ 
 // Listener
@@ -24,12 +21,10 @@ storage_search_string.then(event_storage_search_string, on_error_storage);
 let storage_search_map_entry = browser.storage.local.get("search_map_entry");
 storage_search_map_entry.then(event_storage_search_map_entry, on_error_storage);
 
-let storage_search_config_path = browser.storage.local.get("search_config_path");
-storage_search_config_path.then(event_storage_search_config_path, on_error_storage);
-
 function on_error_storage(error) { 
     console.log(error);
 }
+
 
 /* ************************************************************************* */ 
 // Init search list
@@ -66,6 +61,8 @@ function load_searches() {
             for (var i = 0; i < array_length; i++) {
                 html_list_search(searches[i]);
             }
+            
+            set_search_input_focus();
         });
 }
 load_searches();
@@ -75,11 +72,14 @@ load_searches();
 // Search String 
 
 function set_search_input_focus() {
-    var len = html_search_string.value.length; 
-    if (html_search_string.setSelectionRange) { 
-        html_search_string.focus(); 
-        html_search_string.setSelectionRange(len, len); 
-    }  
+    html_search_string.focus();
+    var search_length = html_search_string.value.length; 
+    var search_value = html_search_string.value;
+    html_search_string.value = "";
+    if (search_length > 0) {
+        html_search_string.value = search_value;
+        html_search_string.setSelectionRange(search_length, search_length);
+    }
 }
 
 function event_storage_search_string(item) {
@@ -89,12 +89,12 @@ function event_storage_search_string(item) {
     else {
         html_search_string.value = "";
     }
-    html_search_string.focus();
+    set_search_input_focus();
 }
 
 function delete_search_text() {
     html_search_string.value = "";
-    html_search_string.focus();
+    set_search_input_focus();
     browser.storage.local.set({ "search_string" : { "value" : "" }});
 }
 
@@ -119,25 +119,6 @@ function event_storage_search_map_entry(item) {
   else {
       apply_search(searches[0]);
   }
-}
-
-
-/* ************************************************************************* */ 
-// Search Config Path
-
-function event_storage_search_config_path(item) {
-    if (item.search_config_path !== undefined) {
-        var search_config_path = item.search_config_path.value;
-        html_file_preview.textContent = search_config_path;
-    }
-    else {
-
-        
-    }
-}
-
-function set_storage_search_config_path(config_path) {
-    browser.storage.local.set({ "search_config_path" : { "value" : config_path }});
 }
 
 
@@ -185,11 +166,11 @@ function global_onkeydown(event) {
         on_selection_change();
     }
     else if (event.keyCode === 38) { // Key: Up
-    html_search_select.selectedIndex--;
-    if (html_search_select.selectedIndex < 0) {
-        html_search_select.selectedIndex = html_search_select.length - 1;
-    }    
-    on_selection_change();
+        html_search_select.selectedIndex--;
+        if (html_search_select.selectedIndex < 0) {
+            html_search_select.selectedIndex = html_search_select.length - 1;
+        }    
+        on_selection_change();
     }
     else if (event.altKey && (event.keyCode === 88)) { // Key: Alt + x
         delete_search_text();
@@ -197,8 +178,8 @@ function global_onkeydown(event) {
 }
 
 function global_onkeyup(event) {
-    // Force focus at end of input text when down key is pressed and focus is moved to beginning of line
-    if (event.keyCode === 38) { // Key: Down
+    // Force focus at end of input text when up key is pressed and focus is moved to beginning of line
+    if (event.keyCode === 38) { // Key: Up
         set_search_input_focus();
     }
 }
@@ -224,20 +205,5 @@ html_search_string.addEventListener("change", function(event) {
     browser.storage.local.set({ "search_string" : { "value" : html_search_string.value }});
 });
 
-/// Listener
-html_file_button.addEventListener("click", function(event) {
-      if (html_file_dialog) {
-        html_file_dialog.click();
-      }
-    },
-    false
-);
-
-/// Listener
-html_file_dialog.addEventListener("change", function() {
-        set_storage_search_config_path(this.files[0]);
-    },
-    false
-);
 
 /* ************************************************************************* */ 
